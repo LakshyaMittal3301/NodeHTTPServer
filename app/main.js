1aa7eaeba99fdac96ee282025843e30387303faa
Lakshya Mittal <lmittal@commvault.com>
2024-02-05 19:15:32 +0530

    Respond with 200

--- app/main.js
+++ app/main.js
@@ -7,9 +7,16 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
-    socket.end();
-    server.close();
+    socket.on('data', (data) => {
+        console.log(`Received data: ${data}`);
+        let response = "HTTP/1.1 200 OK\r\n\r\n";
+        socket.write(reponse);
+        socket.end();
+    })
+    
+    socket.on("close", () => {
+        console.log('Closing the socket and ending the server');
+        socket.end();
   });
 });
 
