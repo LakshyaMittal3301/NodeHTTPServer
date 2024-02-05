6558397a62c9569f778b56e7f82873fd998271c8
Lakshya Mittal <lmittal@commvault.com>
2024-02-05 23:44:30 +0530

    Post a file

--- app/main.js
+++ app/main.js
@@ -5,92 +5,157 @@ const Path = require('path');
 const fs = require('fs');
 
 class HTTPRequest{
-    
-    response;
+	
+	response;
+	
+	httpMethod;
+	path;
+	httpVersion;
+	headers;
+	body;
 
-    constructor(req){
-        let lines = req.split('\r\n');
-        let startLine = lines[0];
-        let hostLine = lines[1];
-        let userAgentLine = lines[2];
+	#readHeaderLine(line){
+		let headerKey = "";
+		let headerVal = "";
+		let i = 0;
+		while(line[i] !== ':') headerKey += line[i++];
+		i+=2;
+		while(i < line.length) headerVal  += line[i++];
 
-        let arr = startLine.split(' ');
-        this.httpMethod = arr[0];
-        this.path = arr[1];
-        this.httpVersion = arr[2];
-        
-        if(hostLine)
-        this.host = hostLine.split(' ')[1];
+		return {headerKey, headerVal};
+	}
 
-        if(userAgentLine)
-        this.userAgent = userAgentLine.split(' ')[1];
+	constructor(req){
+		let lines = req.split('\r\n');
+		let startLine = lines[0];
 
-        return this;
-    }
-    
-    getResponse(){
-        switch(this.httpMethod){
-            case 'GET':
-                this.handleGet();
-                break; 
-        }
-        return this.response;
-    }
-    
-    handleGet(){
-        let path = this.parseHTTPPath(this.path);
-        if(path[0] == ''){
-            this.response = `${this.httpVersion} 200 OK\r\n\r\n`;
-        } else if(path[0] == 'echo'){
-            this.echoResource();
-        }else if(path[0] == 'user-agent'){
-            this.userAgentResource();
-        }else if(path[0] == 'files'){
-            this.getFilesResource();
-        }
-        else{
-            this.setResponseTo404();
-        }
-    }
+		let arr = startLine.split(' ');
+		this.httpMethod = arr[0];
+		this.path = arr[1];
+		this.httpVersion = arr[2];
 
-    parseHTTPPath(path){
-        return path.split('/').slice(1);
-    }
+		let i = 0;
+		while(lines[i] !== ''){
+			let { headerKey, headerVal } = this.#readHeaderLine(lines[i]);
+			this.headers[headerKey] = headerVal;
+			i++;
+		}
+		i++;
+		if('Content-Length' in this.headers){
+			this.body = lines[i].slice(0, this.headers['Content-Length']);
+		}
 
-    echoResource(){
-        let message = this.path.slice(6);
-        this.setResponseToContent200(message, 'text/plain');
-    }
+		return this;
+	}
+	
+	getResponse(){
+		switch(this.httpMethod){
+			case 'GET':
+				this.handleGet();
+				break; 
+			case 'POST':
+				this.handlePost();
+				break;
+		}
+		return this.response;
+	}
+	
+	handleGet(){
+		let path = this.parseHTTPPath();
+		switch (path[0]) {
+			case '':
+				this.setResponseToEmpty200();
+				break;
+			
+			case 'echo':
+				this.echoResource();
+				break;
+			
+			case 'user-agent':
+				this.userAgentResource();
+				break;
+			
+			case 'files':
+				this.getFilesResource();
+				break;
+			
+			default:
+				this.setResponseTo404();
+				break;
+		}          
+	}
 
-    userAgentResource(){
-        let message = this.userAgent;
-        this.setResponseToContent200(message, 'text/plain');
-    }
+	handlePost(){
+		let path = this.parseHTTPPath();
+		switch(path[0]){
+			case 'files':
+				this.postFilesResource();
+				break;
+			default:
+				this.setResponseTo404();
+				break;
+		}
+	}
 
-    getFilesResource(){
-        let fileName = this.path.slice(7);
-        let filePath = Path.join(directoryPath, fileName);
-        if(!fs.existsSync(filePath)){
-            this.setResponseTo404();
-            return;
-        }
-        const data = fs.readFileSync(filePath);
-        this.setResponseToContent200(data, 'application/octet-stream'); 
+	parseHTTPPath(){
+		return this.path.split('/').slice(1);
+	}
 
-    }
+	echoResource(){
+		let message = this.path.slice(6);
+		this.setResponseToContent200(message, 'text/plain');
+	}
 
-    setResponseTo404(){
-        this.response = `${this.httpVersion} 404 Not Found\r\n\r\n`;
-    }
+	userAgentResource(){
+		let message = this.headers['User-Agent'];
+		this.setResponseToContent200(message, 'text/plain');
+	}
 
-    setResponseToContent200(content, contentType){
-        let response = `${this.httpVersion} 200 OK\r\n`;
-        response += `Content-Type: ${contentType}\r\n`;
-        response += `Content-Length: ${content.length}\r\n`;
-        response += `\r\n`;
-        response += `${content}\r\n`;
-        this.response = response;
-    }
+	getFilesResource(){
+		let fileName = this.path.slice(7);
+		let filePath = Path.join(directoryPath, fileName);
+		if(!fs.existsSync(filePath)){
+			this.setResponseTo404();
+			return;
+		}
+		const data = fs.readFileSync(filePath);
+		this.setResponseToContent200(data, 'application/octet-stream'); 
+
+	}
+
+	postFilesResource(){
+		let fileName = this.path.slice(7);
+		let filePath = Path.join(directoryPath, fileName);
+		try{
+			fs.writeFileSync(filePath, this.body);
+		}
+		catch(err){
+			console.log(`Error occured while writing to file with path ${filePath}: ${err}`);
+		}
+		this.setResponseTo201();
+	}
+
+	setResponseToEmpty200(){
+		this.response = `${this.httpVersion} 200 OK\r\n\r\n`;
+	}
+
+	setResponseTo201(){
+		let response = `${this.httpVersion} 201 Created\r\n\r\n`;
+	}
+
+	setResponseTo404(){
+		this.response = `${this.httpVersion} 404 Not Found\r\n\r\n`;
+	}
+
+	setResponseToContent200(content, contentType){
+		let response = `${this.httpVersion} 200 OK\r\n`;
+		response += `Content-Type: ${contentType}\r\n`;
+		response += `Content-Length: ${content.length}\r\n`;
+		response += `\r\n`;
+		response += `${content}\r\n`;
+		this.response = response;
+	}
+
 };
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -98,46 +163,46 @@ console.log("Logs from your program will appear here!");
 let directoryPath;
 
 (function initialize(args){
-    if(args.length == 0) return;
-    let flag = args[0].slice(2);
-    if(flag === 'directory'){
-        directoryPath = args[1];        
-        console.log("setting directory", directoryPath);
-    }
+	if(args.length == 0) return;
+	let flag = args[0].slice(2);
+	if(flag === 'directory'){
+		directoryPath = args[1];        
+		console.log("setting directory", directoryPath);
+	}
 })(process.argv.slice(2));
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-    socket.on('data', (data) => {
-        console.log(`Received data: ${data}`);
+	socket.on('data', (data) => {
+		console.log(`Received data: ${data}`);
 
-        let httpObject = new HTTPRequest(data.toString());
-        let response = httpObject.getResponse();
-        console.log(`Response to client: ${response}`);
-        socket.write(response);
+		let httpObject = new HTTPRequest(data.toString());
+		let response = httpObject.getResponse();
+		console.log(`Response to client: ${response}`);
+		socket.write(response);
 
-        socket.end();
-    });
-    
-    socket.on('error', (err) => {
-        console.log(`Error occured: ${err.message}`);
-    });
+		socket.end();
+	});
+	
+	socket.on('error', (err) => {
+		console.log(`Error occured: ${err.message}`);
+	});
 
-    socket.on('end', () => {
-        console.log("Socket connection ended");
-        socket.end();
-    })
+	socket.on('end', () => {
+		console.log("Socket connection ended");
+		socket.end();
+	})
 
-    socket.on("close", (hadError) => {
-        if(hadError){
-            console.log('Closing the socket because of an error');
-        }
-        else{
-            console.log('Closing the socket successfully');
-        }
+	socket.on("close", (hadError) => {
+		if(hadError){
+			console.log('Closing the socket because of an error');
+		}
+		else{
+			console.log('Closing the socket successfully');
+		}
   });
 });
 
 server.listen(PORT, LOCALHOST, () => {
-    console.log(`Listening on ${LOCALHOST}:${PORT}`);
+	console.log(`Listening on ${LOCALHOST}:${PORT}`);
 });
